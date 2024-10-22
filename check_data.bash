a17b7b777186d659a8fcdb0ea4749c8e7b810f42
Sicheol Sung <cdltlehf@gmail.com>
2024-10-22 12:13:26 +0200

    add checking script for train.jsonl files

--- check_data.bash
+++ check_data.bash
@@ -1,6 +1,6 @@
 shopt -s globstar
 
-for f in ${DATA_DIR}/{execution-summary,generation-result,grammar,parsing-result}/**/test.jsonl ${RAW_DATA_DIR}/{grammar,level,testcase}/**/test.jsonl; do
+for f in ${DATA_DIR}/{execution-summary,generation-result,grammar,parsing-result}/**/*.jsonl ${RAW_DATA_DIR}/{grammar,level,testcase}/**/*.jsonl; do
   filename=$(basename -- "$f" ".jsonl")
   if jq '.["name"]' -r $f | diff - "raw-data/problem-list/${filename}.txt" > /dev/null 2>&1; then
     true
